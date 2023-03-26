8b5daea75f3b6ebfa993312402e638763be2f284
Jason Adkins <jasonadkins@gmail.com>
2023-03-26 11:36:20 -0400

    Added countdown to Christmas

--- game.js
+++ game.js
@@ -85,3 +85,18 @@ document
         document.getElementById('check-response').classList.add('is-hidden');
         document.getElementById('invalid-count').classList.add('is-hidden')
     })
+
+let today = new Date();
+let christmasYear = today.getFullYear();
+
+    if (today.getMonth() == 11 && today.getDate() > 25) {
+    christmasYear = christmasYear + 1;
+    }
+
+let christmasDate = new Date(christmasYear, 11, 25);
+let dayMilliseconds = 1000 * 60 * 60 * 24;
+
+let remainingDays = Math.ceil(
+  (christmasDate.getTime() - today.getTime()) /
+   (dayMilliseconds)
+);
